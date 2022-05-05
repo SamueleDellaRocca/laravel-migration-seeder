be4237fed48ae6512990858299c6e82f987a8479
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-05-05 16:52:55 +0200

    css linkato

--- resources/views/TrainList.blade.php
+++ resources/views/TrainList.blade.php
@@ -1,8 +1,23 @@
-<h1>Questi sono i treni che partono oggi, ovvero il 2022-07-05</h1>
-@foreach ($treni as $treno)
-<div>
-    <p>
-        {{ $treno->stazione_di_partenza }} ---> {{ $treno->stazione_di_arrivo }}, il giorno {{ $treno->data_di_partenza }}
-    </p>
-</div>
-@endforeach
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <h1>Questi sono i treni che partono oggi, ovvero il 2022-07-05</h1>
+        @foreach ($treni as $treno)
+        <div>
+            <p>
+                {{ $treno->stazione_di_partenza }} ---> {{ $treno->stazione_di_arrivo }}, il giorno {{ $treno->data_di_partenza }}
+            </p>
+        </div>
+        @endforeach
+</body>
+</html>
+
+
